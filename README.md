3c97f4e9990f46f44ac0b15b92b169c6eef133c6
Sparsh Gupta <sparshg@gmail.com>
2023-11-22 20:22:00 -0800

    Polished raw circuit code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# HybridQuantumClassical-SelfAttention
+# Hybrid Quantum Classical Self Attention
+
+Sample Quantum Self Attention Variational Circuit:
+
+![Screenshot 2023-11-22 at 6.34.41 PM.png](..%2FDesktop%2FScreenshot%202023-11-22%20at%206.34.41%E2%80%AFPM.png)
+
+Motivation from Classical Self Attention Mechanism from paper (Fig. 2): https://arxiv.org/pdf/1711.07971.pdf
+
+![Screenshot 2023-11-10 at 5.23.36 PM.png](..%2FDesktop%2FScreenshot%202023-11-10%20at%205.23.36%E2%80%AFPM.png)
--- quantum_self_attention.py
+++ quantum_self_attention.py
@@ -3,12 +3,8 @@ from qiskit_aer import AerSimulator
 from qiskit.extensions import UnitaryGate
 import numpy as np
 
-# Use Aer's AerSimulator
+# Use AerSimulator
 simulator = AerSimulator()
-
-# Weighted matrix (sample for testing)
-weighted_matrix = np.array([[1, 0], [0, 1]])
-wm_gate = UnitaryGate(weighted_matrix)
 
 # Create a Quantum Circuit
 circuit = QuantumCircuit(4)
@@ -17,28 +13,40 @@ circuit = QuantumCircuit(4)
 cr = ClassicalRegister(2, name='Z')
 circuit.add_register(cr)
 
-# data encoding
-theta = 0.3
+# X data encoding
+x_theta = 0.3
 circuit.ry(0.3, 0)
 
 # Theta embedding
 theta = 0.3
 circuit.ry(0.3, 1)
-circuit.append(wm_gate, [1])
+
+# Unitary Gate - theta embedding
+weighted_matrix_theta = np.array([[1, 0], [0, 1]])
+unitary_theta = UnitaryGate(weighted_matrix_theta, label="W_theta")
+circuit.append(unitary_theta, [1])
 
 # Phi embedding
 phi = 0.3
 circuit.ry(0.5, 2)
-circuit.append(wm_gate, [2])
 
-# Tensor Product (theta and phi)
-circuit.cnot(1, 2)
+# Unitary Gate - phi
+weighted_matrix_phi = np.array([[1, 0], [0, 1]])
+unitary_phi = UnitaryGate(weighted_matrix_phi, label="W_phi")
+circuit.append(unitary_phi, [2])
 
-# Linear embedding
-circuit.append(wm_gate, [3])
+# Tensor Product (theta and phi) - using CNOT gate
+circuit.cx(1, 2)
 
-# Tensor Product (gaussian and linear)
-circuit.cnot(2, 3)
+# Linear (g) embedding
+# Unitary Gate - g
+weighted_matrix_g = np.array([[1, 0], [0, 1]])
+unitary_g = UnitaryGate(weighted_matrix_g, label="W_g")
+circuit.append(unitary_g, [3])
+
+
+# Tensor Product (gaussian and linear) - using CNOT gate
+circuit.cx(2, 3)
 
 # Measure qubit 0 (X) and embedding (qubit 3)
 circuit.measure([0, 3], cr)
